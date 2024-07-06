9a3facde24d7b5dd281eb289b98b58939af0778c
shreejanpandit <shreezanpandit@gmail.com>
2024-07-06 20:07:08 +0545

    routes for create, index and store

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransactionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/transactions/create', [TransactionController::class,'create'])->name('transaction.create');
+Route::get('/', [TransactionController::class,'index'])->name('transaction.index');
+Route::post('/transactions', [TransactionController::class,'store'])->name('transaction.store');
