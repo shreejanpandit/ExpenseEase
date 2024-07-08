4013323cb91781d7622b86269a0b13fbeb85adb8
shreejanpandit <shreezanpandit@gmail.com>
2024-07-08 08:04:09 +0545

    transaction belong to logedin user

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'amount',
+        'description',
+        'type',
+        'transaction_date',
+        'user_id',
+    ];
+
+    public function userData()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
